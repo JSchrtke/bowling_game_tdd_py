46284478f787134e766b191acbc57660eb240688
JSchrtke <johannes.schroetke@gmail.com>
2019-09-14 14:52:17 +0200

    Change test framework

--- test_bowling_game.py
+++ test_bowling_game.py
@@ -1,53 +1,51 @@
-import unittest
+import pytest
 import bowling_game
 
+# setup
+@pytest.fixture
+def game():
+    return bowling_game.BowlingGame()
 
-class TestBowlingGame(unittest.TestCase):
-    def setUp(self):
-        self.g = bowling_game.BowlingGame()
-        return super().setUp()
 
-    def tearDown(self):
-        return super().tearDown()
+# utility functions
+def roll_ntimes_mpins(game, rolls, pins):
+    for i in range(rolls):
+        game.roll(pins)
 
-    def test_gutter_game(self):
-        self.roll_ntimes_mpins(20, 0)
-        self.assertEqual(self.g.score(), 0)
 
-    def test_all_ones(self):
-        self.roll_ntimes_mpins(20, 1)
-        self.assertEqual(self.g.score(), 20)
+def roll_many(game, *pins):
+    for pin in pins:
+        game.roll(pin)
 
-    def test_one_spare(self):
-        # Act
-        self.g.roll(3)
-        self.g.roll(7)
-        self.g.roll(4)
-        self.roll_ntimes_mpins(17, 0)
-        # Assert
-        self.assertEqual(self.g.score(), 18)
 
-    def test_all_spares(self):
-        # Act
-        self.roll_ntimes_mpins(21, 5)
-        # Assert
-        self.assertEqual(self.g.score(), 150)
+# tests
+def test_gutter_game(game):
+    roll_ntimes_mpins(game, 20, 0)
+    assert game.score() == 0
 
-    def test_one_strike(self):
-        # Act
-        self.g.roll(10)
-        self.g.roll(3)
-        self.g.roll(3)
-        self.roll_ntimes_mpins(17, 0)
-        # Assert
-        self.assertEqual(self.g.score(), 22)
 
-    def test_all_strikes(self):
-        # Act
-        self.roll_ntimes_mpins(12, 10)
-        # Assert
-        self.assertEqual(self.g.score(), 300)
+def test_all_ones(game):
+    roll_ntimes_mpins(game, 20, 1)
+    assert game.score() == 20
 
-    def roll_ntimes_mpins(self, rolls, pins):
-        for _ in range(rolls):
-            self.g.roll(pins)
+
+def test_one_spare(game):
+    roll_many(game, 3, 7, 4)
+    roll_ntimes_mpins(game, 17, 0)
+    assert game.score() == 18
+
+
+def test_all_spares(game):
+    roll_ntimes_mpins(game, 21, 5)
+    assert game.score() == 150
+
+
+def test_one_strike(game):
+    roll_many(game, 10, 3, 3)
+    roll_ntimes_mpins(game, 17, 0)
+    assert game.score() == 22
+
+
+def test_all_strikes(game):
+    roll_ntimes_mpins(game, 12, 10)
+    assert game.score() == 300
